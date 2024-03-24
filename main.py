08a696c9090a1a12efa811c1f83e7879c1743660
Anashkin Aleksei <109337222+NewalexOA@users.noreply.github.com>
2024-03-24 11:48:47 +0300

    Update main.py
    add new const

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("My Shooting Game")
+icon = pygame.image.load("icon.png")
+
 running = True
 
 while running:
     pass
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
 pygame.quit()
